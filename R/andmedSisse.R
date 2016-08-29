a4b6d99942c7c9995ae8137e427211e58ebbccba
Risto Hinno <ristohinno@gmail.com>
2016-08-29 19:51:22 +0300

    vigade parandus

--- R/andmedSisse.R
+++ R/andmedSisse.R
@@ -1,7 +1,7 @@
 #' andmete downloadi abifunktsioon
 #' @export
 andmedSisse=function(url="https://www.riigiteenused.ee/api/et/all") {
-  library(rjson)
+  #library(rjson)
   library(jsonlite)
   #loeme andmed sisse
   #fromJSON(url, flatten=T) ei funka
